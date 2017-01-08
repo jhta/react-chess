45563e82d6154544f4e50ceec6edfaeb7c7ef107
jhta <jeisonhs93@gmail.com>
2017-01-08 14:52:13 -0500

    add constants for create default table

---
+++ src/constants/pieces.js
@@ -0,0 +1,38 @@
+/**
+ * Default piece object
+ * @type {Object}
+ */
+export const DEFAULT_PIECE = {
+  figure: null,
+  color: null
+};
+
+/**
+ * name pieces whites
+ * @type {Array}
+ */
+export const PIECES_WHITE= [
+  'rook_black',
+  'bishop_black',
+  'pawn_black',
+  'king_black',
+  'queen_black',
+  'pawn_black',
+  'bioshop_black',
+  'rook_black'
+];
+
+/**
+ * name pieces black
+ * @type {Array}
+ */
+export const PIECES_BLACK= [
+  'rook_black',
+  'bishop_black',
+  'pawn_black',
+  'king_black',
+  'queen_black',
+  'pawn_black',
+  'bioshop_black',
+  'rook_black'
+];
